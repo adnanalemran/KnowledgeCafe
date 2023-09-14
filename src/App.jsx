8a56542e8a831bb8662434cbbf33b92acda98f44
adnanalemran <adnanalemranontor@gmail.com>
2023-09-14 13:31:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className="text-6xl bg-red-300">
+      Knowledge Cafe
+    </h1>
+    </>
+  )
+}
+
+export default App
